5a4c138fc3e8ac0311f647bae2c7570750111dff
김선형 <tjsguddl96@gmail.com>
2023-05-16 16:37:31 +0900

    change structure v1

---
+++ s914_vue_trip_kimsunhyung_yooseungah/src/views/TestView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <header-component></header-component>
+    </div>
+</template>
+
+<script>
+import HeaderComponent from '../components/HeaderComponent.vue'
+export default {
+    name: 'TestView',
+    components :{
+        HeaderComponent
+    }
+}
+</script>
+
+<style>
+
+</style>
