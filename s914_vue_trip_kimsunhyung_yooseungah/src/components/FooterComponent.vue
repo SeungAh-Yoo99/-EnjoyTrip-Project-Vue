6b4bfdbccbb48730daaf2d5190dd0250f56d62c7
유승아 <ysa8497@gmail.com>
2023-05-16 17:54:10 +0900

    footer

---
+++ s914_vue_trip_kimsunhyung_yooseungah/src/components/FooterComponent.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="container">
+        <div id="contacts">
+            <div id="contacts--contact">
+                <p>{{ mail }}</p>
+                <p>{{ phone }}</p>
+            </div>
+            <div id="contacts--social">
+                <div><img src="../assets/logo.png" class="profile"></div>
+                <div><img src="../assets/logo.png" class="profile"></div>
+                <div><img src="../assets/logo.png" class="profile"></div>
+            </div>
+            </div>
+            <div id="mentions">
+            <p>{{ mention }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            mail : 'xavier.seignard+drangies@gmail.com',
+            phone : '06.00.00.00.00',
+            mention : '2018 | mentions légales'
+        };
+    }
+}
+</script>
+
+<style scoped>
+body{
+  display:flex;
+  background-color:#F0F8FF;
+  height:95vh;
+  justify-content:center;
+  align-items:flex-end;
+  font-family:Open Sans;
+}
+
+#container{
+  padding-top:20px;
+  margin-top:20px;
+  width:100vw;
+  color:white;
+  background-color:#292354;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+}
+#contacts{
+  display:flex;
+  justify-content:space-around;
+  align-items:center;
+  width:100%;
+}
+#contacts--contact{
+  text-align:right;
+}
+#contacts--social{
+  width:20%;
+  display : flex;
+  justify-content : space-between;
+}
+#contacts--social div{
+  height:30px;
+  width:30px;
+  background-color:white;
+  border-radius:50%;
+}
+.profile {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+#mentions p {
+  font-size:0.5em;
+}
+</style>
--- s914_vue_trip_kimsunhyung_yooseungah/src/views/TestView.vue
+++ s914_vue_trip_kimsunhyung_yooseungah/src/views/TestView.vue
@@ -1,15 +1,37 @@
 <template>
     <div>
         <header-component></header-component>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        내용을 좀 쓰자 <br>
+        <footer-component></footer-component>
     </div>
 </template>
 
 <script>
 import HeaderComponent from '../components/HeaderComponent.vue'
+import FooterComponent from '../components/FooterComponent.vue'
+
 export default {
     name: 'TestView',
     components :{
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     }
 }
 </script>
